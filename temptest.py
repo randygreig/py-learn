ba85fe9258cc5ae178c79529ad88b9ec68d4e1bf
Randy Greig <randy@greigfamily.ca>
2014-06-21 18:53:40 -0600

    added blink.py

--- temptest.py
+++ temptest.py
@@ -3,6 +3,7 @@
 MIN_TEMP = -100
 temp1 = "/sys/bus/w1/devices/28-00000379fea9/w1_slave"
 temp2 = "/sys/bus/w1/devices/28-00000447416d/w1_slave"
+temp3 = "/sys/bus/w1/devices/28-000004475127/w1_slave"
 
 def read_temp1():
   # open/read/close the file with the temperature
@@ -42,7 +43,27 @@ def read_temp2():
     return temp
   return MIN_TEMP-1
 
+def read_temp3():
+  # open/read/close the file with the temperature
+  temp3file = open(temp3)
+  text = temp3file.read()
+  temp3file.close()
+
+  # split the two lines
+  lines = text.split("\n")
+
+  # make sure the crc is valid
+  if lines[0].find("YES") > 0:
+    #get the 9th (10th) chunk of text and lose the t= bit
+    temp = float((lines[1].split(" ")[9])[2:])
+    # add a decimal point
+    temp /= 1000
+    temp = round(temp,2)
+    return temp
+  return MIN_TEMP-1
+
 print(read_temp1())
 print(read_temp2())
+print(read_temp3())
 
 
