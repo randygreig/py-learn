2e6096aa1c102b7bb91644ab17a6ce0cabf2ae42
Randy Greig <randy@greigfamily.ca>
2015-09-19 14:33:33 -0600

    new learning file

---
+++ pybasic-1.py
@@ -0,0 +1,7 @@
+__author__ = 'Randy'
+if __name__ == "__main__":
+    print("hello world")
+
+item_catalog = {"Blank CD":"7.99", "USB Drive":"12.50", "Keyboard":"18.00"}
+for x in item_catalog:
+    print(x)
